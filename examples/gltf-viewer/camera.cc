ed929582905fc50aaf39b32e8a47ea4680950f5f
Alejandro <ale64bit@gmail.com>
2019-08-22 20:58:29 -0400

    Clamp camera movement to avoid mirroring

--- examples/gltf-viewer/camera.cc
+++ examples/gltf-viewer/camera.cc
@@ -4,16 +4,19 @@
 #include "glm/gtx/quaternion.hpp"
 #include "glm/gtx/transform.hpp"
 
+#include <algorithm>
+
 Camera::Camera(float dist) : dist_(dist), target_(glm::fvec3(0)) {}
 
 void Camera::Update(float dx, float dy, float dz) {
-  x_ -= dx * 0.01f;
-  y_ -= dy * 0.01f;
-  dist_ += dz * 0.1f;
+  x_ -= dx * 0.1f;
+  y_ = std::max(std::min(y_ - dy * 0.1f, 89.f), -89.f);
+  dist_ = std::max(dist_ + dz * 0.1f, .0f);
 }
 
 glm::fvec3 Camera::Eye() const {
-  glm::fmat3 rot = glm::toMat3(glm::quat(glm::fvec3(y_, x_, 0)));
+  glm::fmat3 rot =
+      glm::toMat3(glm::quat(glm::fvec3(glm::radians(y_), glm::radians(x_), 0)));
   return rot * glm::fvec3(0, 0, dist_);
 }
 
